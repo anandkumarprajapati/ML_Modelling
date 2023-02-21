d902b50e0287f03c7eab9599792adcd6c9010fdd
anandkumarprajapati <amateurphilosophy77@gmail.com>
2023-02-21 16:49:38 +0530

    second_commit

--- Tree_Based_Methods.py
+++ Tree_Based_Methods.py
@@ -5,3 +5,6 @@ Created on Tue Feb 21 15:55:53 2023
 @author: anand
 """
 
+import pandas as pd
+
+dataframe= pd.read_csv(r'C:\Users\anand\OneDrive\Desktop\interview prep\ML\Model Deployment\ML_Modelling\temps.csv')
